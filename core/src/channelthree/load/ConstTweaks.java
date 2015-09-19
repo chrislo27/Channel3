96f0d358f6271c0ca25a637def4f8726f86daa96
chrislo27 <kinchrislo@gmail.com>
2015-09-18 20:13:38 -0700

    60 TPS

--- core/src/channelthree/load/ConstTweaks.java
+++ core/src/channelthree/load/ConstTweaks.java
@@ -1,10 +1,12 @@
 package channelthree.load;
 
+import ionium.registry.ConstantsRegistry;
+
 
 public class ConstTweaks {
 
 	public static final void tweakConstants(){
-		
+		ConstantsRegistry.instance().putInt("TICKS", 60);
 	}
 	
 }
