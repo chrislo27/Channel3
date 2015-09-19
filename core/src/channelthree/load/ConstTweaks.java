af181682bf8901fcbf0722d4c4a87864ae45b0df
chrislo27 <kinchrislo@gmail.com>
2015-09-19 08:11:48 -0700

    Hid FPS display by default

--- core/src/channelthree/load/ConstTweaks.java
+++ core/src/channelthree/load/ConstTweaks.java
@@ -1,12 +1,15 @@
 package channelthree.load;
 
 import ionium.registry.ConstantsRegistry;
+import ionium.util.DebugSetting;
 
 
 public class ConstTweaks {
 
 	public static final void tweakConstants(){
 		ConstantsRegistry.instance().putInt("TICKS", 60);
+		
+		DebugSetting.showFPS = false;
 	}
 	
 }
