8d73f07dbfa61415090cd42910b638cadf047c65
chrislo27 <kinchrislo@gmail.com>
2015-09-30 16:20:06 -0700

    Loading rain textures

--- core/src/channelthree/load/AssetLoader.java
+++ core/src/channelthree/load/AssetLoader.java
@@ -1,18 +1,23 @@
 package channelthree.load;
 
+import ionium.animation.Animation;
+import ionium.registry.handler.IAssetLoader;
+import ionium.util.AssetMap;
+
 import java.util.HashMap;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 
-import ionium.animation.Animation;
-import ionium.registry.handler.IAssetLoader;
-
-
 public class AssetLoader implements IAssetLoader {
 
 	@Override
 	public void addManagedAssets(AssetManager manager) {
+		// raindrops
+		manager.load(AssetMap.add("raindrop", "images/rain/raindrop.png"), Texture.class);
+		for(int i = 0; i < 5; i++){
+			manager.load(AssetMap.add("raindrop_splat" + i, "images/rain/splat" + i + ".png"), Texture.class);
+		}
 	}
 
 	@Override
