a597e1a0fb931aa17c2e6071686135b1a661913a
chrislo27 <kinchrislo@gmail.com>
2015-09-30 16:19:58 -0700

    Added rain effect class

---
+++ core/src/channelthree/util/RainEffect.java
@@ -0,0 +1,34 @@
+package channelthree.util;
+
+import ionium.registry.AssetRegistry;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.MathUtils;
+
+public final class RainEffect {
+
+	/**
+	 * 
+	 * @param batch
+	 * @param drops
+	 * @param splats
+	 * @param partToSplat bottom percentage of where to splat
+	 */
+	public static void rainEffect(Batch batch, int drops, int splats, float partToSplat) {
+		Texture raindrop = AssetRegistry.getTexture("raindrop");
+
+		for (int i = 0; i < drops; i++) {
+			batch.draw(raindrop, Gdx.graphics.getWidth() * MathUtils.random(),
+					Gdx.graphics.getHeight() * MathUtils.random());
+		}
+
+		for (int i = 0; i < splats; i++) {
+			batch.draw(AssetRegistry.getTexture("raindrop_splat" + MathUtils.random(0, 4)),
+					Gdx.graphics.getWidth() * MathUtils.random(), Gdx.graphics.getHeight()
+							* MathUtils.random() * partToSplat);
+		}
+	}
+
+}
