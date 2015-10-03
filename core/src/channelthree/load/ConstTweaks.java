7276ee68e7b2e67f660211538e19f0299825b2a9
chrislo27 <kinchrislo@gmail.com>
2015-10-03 10:35:40 -0700

    Set the version getter's URL to grab from

--- core/src/channelthree/load/ConstTweaks.java
+++ core/src/channelthree/load/ConstTweaks.java
@@ -3,13 +3,16 @@ package channelthree.load;
 import ionium.registry.GlobalVariables;
 import ionium.util.DebugSetting;
 
-
 public class ConstTweaks {
 
-	public static final void tweakConstants(){
+	public static final void tweakConstants() {
 		GlobalVariables.instance().putInt("TICKS", 60);
-		
+		GlobalVariables
+				.instance()
+				.putString("VERSION_URL",
+						"https://raw.githubusercontent.com/chrislo27/VersionPlace/master/Channel3-version.txt");
+
 		DebugSetting.showFPS = false;
 	}
-	
+
 }
