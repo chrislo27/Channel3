90addc219a574dfd8bbea5258cef33330c9b4262
chrislo27 <kinchrislo@gmail.com>
2015-10-05 08:04:47 -0700

    Added some save and unload methods with error checking

--- core/src/channelthree/savefile/SaveFile.java
+++ core/src/channelthree/savefile/SaveFile.java
@@ -4,27 +4,49 @@ import channelthree.Settings;
 
 import com.badlogic.gdx.Preferences;
 
-
 public class SaveFile {
 
 	private int currentlyLoaded = -1;
-	
-	public SaveFile(){
-		
+	Preferences pref = null;
+
+	public SaveFile() {
+
 	}
-	
-	public SaveFile load(int index){
-		Preferences pref = Settings.getPref("savefile-" + index);
+
+	public SaveFile load(int index) {
+		pref = Settings.getPref("savefile-" + index);
+
+		currentlyLoaded = index;
+		return this;
+	}
+
+	public SaveFile save() {
+		if (!hasLoaded()) throw new IllegalStateException(
+				"Cannot save savefile when nothing is loaded yet! (index: " + currentlyLoaded
+						+ ", pref null?: " + (pref == null) + ")");
+		
+		pref.flush();
 		
 		return this;
 	}
 	
-	public int getCurrentlyLoadedIndex(){
+	public SaveFile unload(){
+		if (!hasLoaded()) throw new IllegalStateException(
+				"Cannot unload savefile when nothing is loaded yet! (index: " + currentlyLoaded
+						+ ", pref null?: " + (pref == null) + ")");
+		
+		pref = null;
+		currentlyLoaded = -1;
+		
+		return this;
+	}
+
+	public int getCurrentlyLoadedIndex() {
 		return currentlyLoaded;
 	}
-	
-	public boolean hasLoaded(){
-		return currentlyLoaded != -1;
+
+	public boolean hasLoaded() {
+		return currentlyLoaded != -1 && pref != null;
 	}
-	
+
 }
